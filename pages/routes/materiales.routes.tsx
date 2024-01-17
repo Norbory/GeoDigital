3e50f063f07c6b87badcc59b6fcf8c55603ba6f0
Norbory <mandros.angelof@pucp.edu.pe>
2024-01-16 21:14:41 -0500

    Added button and api element

--- pages/routes/materiales.routes.tsx
+++ pages/routes/materiales.routes.tsx
@@ -3,10 +3,11 @@ import Carta from '../../components/carta';
 import { type Images } from '../../types/apiImages';
 
 export default function Materiales(){
-  const [images, setImages] = useState([]);	
-  const [stock,setStock] = useState(15);
+  const [images, setImages] = useState([]);
+  const [stock,setStock] = useState<number>(15);
+  const [busqueda,setBusqueda] = useState<string>('jimmy+butler');
 
-  const hiper = 'https://real-time-image-search.p.rapidapi.com/search?query=jimmy&region=us';
+  const hiper = `https://real-time-image-search.p.rapidapi.com/search?query=${busqueda}&region=us`;
   const options = {
     method: 'GET',
     headers: {
@@ -22,7 +23,7 @@ export default function Materiales(){
     const handleAumentar = () => {
       setStock(stock+1);
     };
-  
+
     const handleSoporte = async () => {
       try {
         const response = await fetch(hiper, options);
@@ -36,9 +37,20 @@ export default function Materiales(){
       }
     };
 
+    const handleBusqueda = () => {
+      const key = document.getElementById("nombre");
+      const busqueda = key.textContent ? key.textContent : "jimmy+butler";
+      setBusqueda(busqueda)
+      handleSoporte();
+    };
+
   return(
       <div>
           <h2>MATERIALES</h2>
+          <input style={{margin: "10px 20px"}} type="text" name="nombre" placeholder='mango' id="nombre"/>
+          <button onClick={()=>handleBusqueda()}>
+            Añadir material
+          </button>
           <Carta
               nombre="Tuberia de acero de 4 pulgadas"
               url={images[0] ? images[0].id : "https://www.turflexperu.com/wp-content/uploads/2018/12/Tubo-PVC-Agua-1.jpg"}
